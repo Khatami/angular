21866f73f1373710108158733eb92eaa9b2b3c63
hamedkhatami <hamedkhatami@outlook.com>
2022-07-04 15:34:07 +0430

    feat (http.service): add http.service to the project

--- src/app/http/pages/communication/communication.component.ts
+++ src/app/http/pages/communication/communication.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../services/http.service';
 
 @Component({
   selector: 'app-communication',
   templateUrl: './communication.component.html',
-  styleUrls: ['./communication.component.css']
+  styleUrls: ['./communication.component.css'],
+  providers: [HttpService]
 })
 export class CommunicationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpService: HttpService) { }
 
   ngOnInit(): void {
   }
-
 }
